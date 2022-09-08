7f77337282c64c5541ce2c4aa631b596d0275200
Bellsblog <tabansiamara@gmail.com>
2022-09-08 11:47:51 -0700

    cc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char new_string[] = "\"
+		Programming is like building a multlingual puzzle";
+
+	puts(new_string);
+
+	return(0);
+}
