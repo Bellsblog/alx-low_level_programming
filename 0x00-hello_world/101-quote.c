76e8d213030078181479dd3087a67a85a281d6bd
Bellsblog <tabansiamara@gmail.com>
2022-09-08 19:24:02 -0700

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd .h>
-
+#include <unistd.h>
 /**
  * main -Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	write(2, "and that pieace of art is useful\" - Dora Korpar, 2015-10-19\N", 59);
-	return (1);
-}
+
+{write(1, "and that pieace of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+	
